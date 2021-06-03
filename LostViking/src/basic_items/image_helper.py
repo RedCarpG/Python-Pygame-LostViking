ed66ea8c2d6775c176773dfc32e4bcd2590532e9
RedCarpG <37827381+NatakuG@users.noreply.github.com>
2021-06-04 00:48:09 +0200

    ! Separate image_helper.py form entity.py

---
+++ LostViking/src/basic_items/image_helper.py
@@ -0,0 +1,54 @@
+from abc import abstractmethod
+
+
+class SingleImageHelper(object):
+    # A Dictionary of lists of images
+    _IMAGE = None
+    # Flag is set to True once the _IMAGE is initialized
+    _INIT_FLAG = False
+
+    def __init__(self):
+        if not self._INIT_FLAG:
+            self.init_image()
+        self.image = self._IMAGE()
+
+    @classmethod
+    @abstractmethod
+    def init_image(cls):
+        pass
+
+
+class LoopImageHelper(object):
+    """
+    A Helper for drawing Entities by looping in its image list
+    """
+    # A Dictionary of lists of images
+    _IMAGE = {"Base": [None]}
+    # Flag is set to True once the _IMAGE is initialized
+    _INIT_FLAG = False
+
+    def __init__(self):
+        if not self._INIT_FLAG:
+            self.init_image()
+        self._image_switch = 0
+        self._image_switch_count = 0
+        self._main_image_type = self._IMAGE["Base"]
+        self.image = self._main_image_type[self._image_switch]
+
+    # Switch image
+    def _switch_image(self, switch_rate=5):
+        """ Switch image function should be called """
+        if self._image_switch_count >= switch_rate:
+            self._image_switch = (self._image_switch + 1) % len(self._main_image_type)
+            self.image = self._main_image_type[self._image_switch]
+            self._image_switch_count = 0
+        else:
+            self._image_switch_count += 1
+
+    def _set_image_type(self, image_type):
+        self._main_image_type = self._IMAGE[image_type]
+
+    @classmethod
+    @abstractmethod
+    def init_image(cls):
+        pass
--- LostViking/src/items/entity.py
+++ LostViking/src/items/entity.py
@@ -1,44 +1,9 @@
 """
 General entities for inheriting by classes
 """
-from abc import abstractmethod, ABC
 import pygame
-
-
-class LoopImageHelper(object):
-    """
-    A Helper for drawing Entities by looping in its image list
-    """
-    # A Dictionary of lists of images
-    _IMAGE = {"Base": [None]}
-    # Flag is set to True once the _IMAGE is initialized
-    _INIT_FLAG = False
-
-    def __init__(self):
-        if not self._INIT_FLAG:
-            self.init_image()
-        self._image_switch = 0
-        self._main_image_type = self._IMAGE["Base"]
-        self.image = self._main_image_type[self._image_switch]
-        self._image_switch_count = 0
-
-    # Switch image
-    def _switch_image(self, switch_rate=5):
-        """ Switch image function should be called """
-        if self._image_switch_count >= switch_rate:
-            self._image_switch = (self._image_switch + 1) % len(self._main_image_type)
-            self.image = self._main_image_type[self._image_switch]
-            self._image_switch_count = 0
-        else:
-            self._image_switch_count += 1
-
-    def _set_image_type(self, image_type):
-        self._main_image_type = self._IMAGE[image_type]
-
-    @classmethod
-    @abstractmethod
-    def init_image(cls):
-        pass
+from abc import ABC
+from ..basic_items.image_helper import LoopImageHelper
 
 
 class MovableEntity(LoopImageHelper, ABC):
