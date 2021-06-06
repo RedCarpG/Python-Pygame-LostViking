fb7bf398df290e9339df32f61cec62dc0feb20be
RedCarpG <512828239@qq.com>
2021-06-06 02:07:38 +0200

    add groups.py for pygame.sprite.Group

---
+++ LostViking/src/groups.py
@@ -0,0 +1,14 @@
+
+import pygame
+
+Player1_G = pygame.sprite.GroupSingle(None)
+Player2_G = pygame.sprite.GroupSingle(None)
+
+Player_Bullet_G = pygame.sprite.Group()
+
+Player_NucBomb_G = pygame.sprite.GroupSingle(None)
+NucBomb_Explosion_G = pygame.sprite.GroupSingle(None)
+
+Enemy_G = pygame.sprite.Group()
+Enemy_Bullet_G = pygame.sprite.Group()
+
