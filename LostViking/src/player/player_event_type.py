bc9da3ae0d1f6af1538c9c1034f489f9e50b7615
RedCarpG <512828239@qq.com>
2021-06-06 02:08:07 +0200

    add event_type file

---
+++ LostViking/src/player/player_event_type.py
@@ -0,0 +1,7 @@
+import pygame
+
+EVENT_PLAYER1_SHOOT = pygame.event.custom_type()#EM.add_event("player1_shoot")
+EVENT_PLAYER1_BOMB = pygame.event.custom_type()#EM.add_event("player1_boom")
+EVENT_PLAYER2_SHOOT = pygame.event.custom_type()#EM.add_event("player2_shoot")
+EVENT_PLAYER2_BOMB = pygame.event.custom_type()#EM.add_event("player2_boom")
+EVENT_BOMB_EXPLODE = pygame.event.custom_type()#EM.add_event("bomb_explode")
