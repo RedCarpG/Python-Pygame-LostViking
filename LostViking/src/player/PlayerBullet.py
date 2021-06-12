b4f68afeb994fbbbc92f809ceaf0ffc9c25dcf5d
RedCarpG <37827381+NatakuG@users.noreply.github.com>
2021-06-12 07:05:36 +0200

    Change on PlayerBullet.py

--- LostViking/src/player/PlayerBullet.py
+++ LostViking/src/player/PlayerBullet.py
@@ -6,26 +6,12 @@ from abc import ABC
 import pygame
 
 from ..generic_loader.image_loader import load_image
-from ..generic_items.bullet import BasicBullet
+from ..generic_items.BasicBullet import BasicBullet, BasicSpinBullet
 from ..groups import Player_Bullet_G
+from LostViking.src.generic_items.ImageHelper import SingleImageHelper
 
 
-class PlayerBasicBullet(BasicBullet, ABC):
-    _MAX_LEVEL = 3
-
-    def __init__(self, init_position: (list, tuple),
-                 speed: list[int, int],
-                 damage: int):
-        BasicBullet.__init__(self, init_position=init_position,
-                             speed=speed)
-        self.damage = damage
-
-    @classmethod
-    def get_max_level(cls) -> int:
-        return cls._MAX_LEVEL
-
-
-class PlayerBullet1(PlayerBasicBullet):
+class PlayerBullet1(SingleImageHelper, BasicBullet):
     """ Player's Bullet 1 type,
     -> Implements StraightBullet class :
             -> SingleImageHelper
@@ -33,13 +19,16 @@ class PlayerBullet1(PlayerBasicBullet):
     + Created by shoot() from PlayerPlane
     - Deleted by self.hit() or self.kill() in self.update()
     """
+    _MAX_LEVEL = 0
 
     def __init__(self, init_position: (list, tuple),
                  speed: list[int, int]):
-        PlayerBasicBullet.__init__(self, init_position=init_position,
-                                   speed=speed,
-                                   damage=150)
+        SingleImageHelper.__init__(self)
+        BasicBullet.__init__(self, init_position=init_position,
+                             speed=speed)
         pygame.sprite.Sprite.__init__(self, Player_Bullet_G)
+        self.damage = 150
+        self.add(Player_Bullet_G)
 
     def _trans_image(self, angle: int) -> None:
         temp = self.rect.center
@@ -49,24 +38,36 @@ class PlayerBullet1(PlayerBasicBullet):
 
     @classmethod
     def shoot_bullets(cls, position: list[int, int], level: int):
+
         position1 = position[0] - 10, position[1]
         position2 = position[0] + 10, position[1]
         cls(position1, [0, -17])
         cls(position2, [0, -17])
+
         if level >= 2:
             position1 = position[0] - 15, position[1]
             position2 = position[0] + 15, position[1]
             cls(position1, [-5, -17])._trans_image(angle=20)
             cls(position2, [5, -17])._trans_image(angle=-20)
+
         if level >= 3:
             cls(position1, [-10, -17])._trans_image(angle=30)
             cls(position2, [10, -17])._trans_image(angle=-30)
 
     @classmethod
-    def init_image(cls) -> None:
+    def get_max_level(cls) -> int:
+        return cls._MAX_LEVEL
+
+    @classmethod
+    def _init_image(cls) -> None:
         cls._IMAGE = load_image("PlayerPlane/bullet.png")
 
         cls._INIT_FLAG_IMAGE = True
+
+    @classmethod
+    def init(cls):
+        cls._init_image()
+        cls._MAX_LEVEL = 3
 
 
 # TODO Bullet2
@@ -116,12 +117,17 @@ class PlayerBullet2(BasicBullet):
         return cls._MAX_LEVEL
 
     @classmethod
-    def init_image(cls) -> None:
+    def _init_image(cls) -> None:
         cls._IMAGE = load_image("PlayerPlane/bullet.png")
 
         cls._INIT_FLAG_IMAGE = True
 
+    @classmethod
+    def init(cls):
+        cls._init_image()
+        cls._MAX_LEVEL = 3
+
 
 def init_player_bullet():
-    PlayerBullet1.init_image()
-    #PlayerBullet2.init_image()
+    PlayerBullet1.init()
+    #PlayerBullet2.init()
