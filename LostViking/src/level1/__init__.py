72c32fff5e192858c7a4c462f850ed0694e3e5a6
RedCarpG <37827381+NatakuG@users.noreply.github.com>
2021-06-14 08:54:14 +0200

    add import

--- LostViking/src/level1/__init__.py
+++ LostViking/src/level1/__init__.py
@@ -2,6 +2,7 @@ from .Scout import EnemyScout
 from .Phoenix import EnemyPhoenix
 from .level1_event_type import *
 from .level1_event_handler import *
+from .level1_group import *
 
 
 def level1_init():
