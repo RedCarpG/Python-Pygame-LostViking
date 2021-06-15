c9ba6d180569287d2651f0db4b2bee74ff003c20
RedCarpG <512828239@qq.com>
2021-06-15 13:37:09 +0200

    Add method

--- LostViking/src/generic_items/SoundHelper.py
+++ LostViking/src/generic_items/SoundHelper.py
@@ -4,11 +4,16 @@ import warnings
 
 
 class SoundHelper(object):
+    _SOUND = None
 
     def __init__(self):
         if not hasattr(self, "_INIT_FLAG_SOUND") or not self._INIT_FLAG_SOUND:
             warnings.warn("!!! ERROR: _SOUND value is not set! {}".format(self.__name__))
             self._init_sound()
+
+    def _play_sound(self, label):
+        self._SOUND[label].stop()
+        self._SOUND[label].play()
 
     @classmethod
     @abstractmethod
