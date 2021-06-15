b41369e2d856a593df3e63e81db75eb669131612
RedCarpG <512828239@qq.com>
2021-06-15 14:20:02 +0200

    Add some change

--- LostViking/src/level1/__init__.py
+++ LostViking/src/level1/__init__.py
@@ -1,5 +1,5 @@
 from .Scout import EnemyScout
-from .Phoenix import EnemyPhoenix, BulletPhoenix
+from .Phoenix import EnemyPhoenix, BulletPhoenix, Shield
 from .level1_event_type import *
 from .level1_event_handler import *
 from .level1_group import *
@@ -9,3 +9,4 @@ def level1_init():
     EnemyScout.init()
     EnemyPhoenix.init()
     BulletPhoenix.init()
+    Shield.init()
