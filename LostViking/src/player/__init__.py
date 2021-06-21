e9ee931e8f47019f1490f2aef005e2655bfc9a17
RedCarpG <512828239@qq.com>
2021-06-21 14:05:34 +0200

    Fix __init__

--- LostViking/src/player/__init__.py
+++ LostViking/src/player/__init__.py
@@ -1,4 +1,7 @@
 from .player_event_handler import detect_player_event, detect_key_pressed
 from .player_configuration import init_player
 from .PlayerPlane import create_player
-from .player_interface import *
+from .player_interface import set_player_bullet_type, \
+    player_upgrade, get_level, \
+    add_nuc_bomb, dec_nuc_bomb, get_nuc_count, \
+    add_life, dec_life, get_life_count
