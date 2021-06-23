b4bee2b32c444139868944cd537f959208fa51ce
RedCarpG <512828239@qq.com>
2021-06-23 14:28:45 +0200

    little update on player_test.py

--- test/player_test.py
+++ test/player_test.py
@@ -1,26 +1,28 @@
 from LostViking.src.player import *
+from LostViking.src.ingame_items import *
+from LostViking.src.ingame_items.SupplyLife import *
 from LostViking.src.groups import *
 
 from LostViking.src.constants import *
 from pygame.locals import *
-from LostViking.src.generic_loader.color import *
+import pygame
 
 
 def test_player():
     # Init Environment
     pygame.init()
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(SCREEN.get_s())
+    screen = pygame.display.set_mode(SCREEN_SIZE)
 
     # Init Player
     init_player()
-    player1 = create_player()
+    init_supply()
+    player1, _ = create_player()
     set_player_bullet_type(player1, 1)
     player_upgrade(player1)
-    player_upgrade(player1)
     add_nuc_bomb()
-    add_nuc_bomb()
-    add_nuc_bomb()
+
+    s = SupplyLife(position=(500, 500))
 
     def test_key_event():
         if event.key == K_x:
@@ -42,10 +44,12 @@ def test_player():
         detect_key_pressed(player1)
 
         screen.fill(BLACK)
+        Supply_G.update()
         Player_NucBomb_G.update()
         Player_Bullet_G.update()
         Player1_G.update()
         Player_NucBomb_G.update()
+        Supply_G.draw(screen)
         Player_Bullet_G.draw(screen)
         Player_NucBomb_G.draw(screen)
         Player1_G.draw(screen)
