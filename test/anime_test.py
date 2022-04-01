a7eaf82dd5ac548c40aee31da8927e8dfc259d14
RedCarpG <redcarpg@gmail.com>
2022-04-01 19:16:40 +0200

    update: test files

---
+++ test/anime_test.py
@@ -0,0 +1,47 @@
+
+import pygame
+from pygame.locals import *
+from pygame.display import set_caption
+from pygame.sprite import Group
+
+from src.helper.image import load_image
+from src.setting import *
+from src.util import *
+from src.game.animation.AnimSprite import AnimSprite
+
+
+def test_anime():
+    # Init Environment
+    pygame.init()
+    set_caption("Animation Test")
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+
+    images = {
+        "BASE": load_image("PlayerPlane/VikingBody.png", transparency=True),
+        "IDLE": load_image("PlayerPlane/VikingIdle.png", transparency=True)
+    }
+
+    anime_sprites = Group()
+    sprite = AnimSprite(images)
+    anime_sprites.add(sprite)
+    sprite.rect.move_ip(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            # --------------- Key Down Events ---------------
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        screen.fill(BLACK)
+        sprite.update()
+        anime_sprites.draw(screen)
+        # Display
+        pygame.display.flip()
+
+        clock.tick(60)
--- test/player_test.py
+++ test/player_test.py
@@ -1,69 +1,129 @@
-from LostViking.src.player import *
-from LostViking.src.ingame_items import *
-from LostViking.src.ingame_items.SupplyLife import *
-from LostViking.src.groups import *
-
-from LostViking.src.constants import *
 from pygame.locals import *
 import pygame
+import pytest
+
+from src.game.groups import *
+from src.game.player import *
+
+from src.helper.font import *
+from src.game.ui import Scoreboard
+from src.setting import *
+
+
+class Testboard:
+
+    def load_testboard(self):
+        load_font("arialbd.ttf", 25, "Testboard")
+
+    def __init__(self, surface, player) -> None:
+        self.load_testboard()
+        self.surface = surface
+        self.player = player
+        self.Text_G = {
+            'score': FontEntity(self.surface, get_font("Testboard"),
+                                f"Score: {self.player.score}", (0, 30), color=COLOR.WHITE),
+            'life': FontEntity(self.surface, get_font("Testboard"),
+                               f"Life: {self.player.life}", (0, 60), color=COLOR.RED),
+            'level': FontEntity(self.surface, get_font("Testboard"),
+                                f"Level: {self.player.level}", (0, 90), color=COLOR.WHITE),
+            'bomb': FontEntity(self.surface, get_font("Testboard"),
+                               f"Bomb: {self.player.bomb_count}", (0, 120), color=COLOR.WHITE),
+            'bullets': FontEntity(self.surface, get_font("Testboard"),
+                                  f"Bullets: {len(G_Player_Bullet)}", (0, 150), color=COLOR.WHITE),
+            'health': FontEntity(self.surface, get_font("Testboard"),
+                                 f"HP: {self.player.health}", (0, 180), color=COLOR.RED),
+        }
+
+    def update(self):
+        self.Text_G["score"].change_text(f"Score: {self.player.score}")
+        self.Text_G["life"].change_text(f"Life: {self.player.life}")
+        self.Text_G["health"].change_text(f"health: {self.player.health}")
+        self.Text_G["level"].change_text(f"Level: {self.player.level}")
+        self.Text_G["bomb"].change_text(f"Bomb: {self.player.bomb_count}")
+        self.Text_G["bullets"].change_text(f"Bullets: {len(G_Player_Bullet)}")
+
+    def blit(self):
+        for _, text in self.Text_G.items():
+            text.blit()
+
+
+class TestGame:
+    def __init__(self, screen) -> None:
+        load_asset_player()
+        self.screen = screen
+        self.player = PlayerViking(pos=None)
+        G_Player1.add(self.player)
+
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+        self.testboard = Testboard(screen, self.player)
+
+    def event(self):
+
+        def test_key_event():
+            if event.key == K_k:
+                self.player.hit(100)
+            elif event.key == K_z:
+                self.player.level_up()
+            elif event.key == K_x:
+                self.player.level_down()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.running = False
+            # --------------- Key Down Events ---------------
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.running = False
+                test_key_event()
+            detect_player_event(event, player1=self.player)
+
+        detect_key_pressed(self.player)
+
+    def run(self):
+
+        while self.running:
+            self.event()
+
+            self.screen.fill(COLOR.BLACK)
+            G_Player_Bullet.update()
+            G_Player1.update()
+            G_Bomb.update()
+            G_Effect.update()
+            self.testboard.update()
+
+            G_Bomb.draw(self.screen)
+            G_Player_Bullet.draw(self.screen)
+            G_Player1.draw(self.screen)
+            G_Effect.draw(self.screen)
+            self.testboard.blit()
+            # Display
+            pygame.display.flip()
+
+            self.clock.tick(60)
 
 
 def test_player():
     # Init Environment
     pygame.init()
-    clock = pygame.time.Clock()
+    pygame.mixer.init()
+    pygame.font.init()
+    pygame.display.set_caption("Player Test")
+
     screen = pygame.display.set_mode(SCREEN_SIZE)
 
-    # Init Player
-    init_player()
-    init_supply()
-    player1, _ = create_player()
-    set_player_bullet_type(player1, 1)
-    player_upgrade(player1)
-    add_nuc_bomb()
+    game = TestGame(screen=screen)
+    game.run()
 
-    s = SupplyLife(position=(500, 500))
-
-    def test_key_event():
-        if event.key == K_x:
-            player1.hit(100)
-
-    running = True
-    while running:
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-            # --------------- Key Down Events ---------------
-            elif event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-                test_key_event()
-            detect_player_event(event, player1=player1)
-
-        detect_key_pressed(player1)
-
-        screen.fill(BLACK)
-        Supply_G.update()
-        Player_NucBomb_G.update()
-        Player_Bullet_G.update()
-        Player1_G.update()
-        Player_NucBomb_G.update()
-        Supply_G.draw(screen)
-        Player_Bullet_G.draw(screen)
-        Player_NucBomb_G.draw(screen)
-        Player1_G.draw(screen)
-        Player_NucBomb_G.draw(screen)
-        # Display
-        pygame.display.flip()
-
-        clock.tick(60)
+    pygame.font.quit()
+    pygame.mixer.quit()
+    pygame.quit()
 
 
 if __name__ == "__main__":
     test_player()
 
-    pygame.quit()
     import sys
 
     sys.exit()
