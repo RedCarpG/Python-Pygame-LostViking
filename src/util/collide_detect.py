34a846b29aca52d8d8a88637f04f663290d496a3
RedCarpG <redcarpg@gmail.com>
2022-04-21 23:52:22 +0200

    update: collision detect function

--- src/util/collide_detect.py
+++ src/util/collide_detect.py
@@ -1,8 +1,10 @@
 from pygame.sprite import collide_rect_ratio, collide_rect
 
 
-def collide_detect(a, b):
-    if collide_rect(a, b):
-        if collide_rect_ratio(0.5)(a, b):
-            return True
-    return False
+def collide_detect(ratio=0.8):
+    def collide_func(a, b):
+        if collide_rect(a, b):
+            if collide_rect_ratio(ratio)(a, b):
+                return True
+        return False
+    return collide_func
